d4c0f2a9bc198b7a6012e1eaaa8bb7463324aac9
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-05-28 15:51:21 -0500

    completed app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Image from "./Image";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+        <div className="App">
+            <Image url={"https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1920%2C1920&ssl=1"}/>
+        </div>
+    )
 }
 
 export default App;
---
+++ app-5/src/Image.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Image(props) {
+    return (
+        <div>
+            <img src={props.url} alt='' />
+        </div>
+    )
+}
+
+export default Image
