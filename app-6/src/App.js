b278f9b8b1d19df6a8f964c756e8cf4fc06d9a29
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-05-28 16:08:44 -0500

    completed app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Todo from './Todo';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [input, setInput] = useState('')
+    const [tasks, setTasks] = useState([])
+
+    const displayTasks = tasks.map((element, index) => {
+        return <Todo task={element} key={index} />
+    })
+
+    return (
+        <div className="App">
+            <h1>My to-do list:</h1>
+            <input onChange={e => setInput(e.target.value)}></input>
+            <button onClick={() => setTasks([...tasks, input])}>Add</button>
+            {displayTasks}
+        </div>
+    )
 }
 
 export default App;
